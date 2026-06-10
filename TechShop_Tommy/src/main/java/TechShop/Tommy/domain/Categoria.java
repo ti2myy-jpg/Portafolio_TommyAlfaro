75fd289ca18b121354b11fb6580af211993e650d
ti2myy-jpg <ti2myy@gmail.com>
2026-06-09 18:53:55 -0600

    Commit_Semana 3
    Commit final de semana 3, con SQL trabajando

--- TechShop_Tommy/src/main/java/TechShop/Tommy/domain/Categoria.java
+++ TechShop_Tommy/src/main/java/TechShop/Tommy/domain/Categoria.java
@@ -1,44 +1,44 @@
 /*
 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
 * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-*/
-//package TechShop.Tommy.domain;
-// 
-// 
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//import jakarta.validation.constraints.NotNull;
-//import jakarta.validation.constraints.Size;
-//import java.io.Serializable;
-//import lombok.Data;
-//
-// 
-//@Data
-//@Entity
-//@Table(name = "categoria")
-//public class Categoria implements Serializable {
-// 
-//    private static final long serialVersionUID = 1L;
-// 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "id_categoria")
-//    private Integer idCategoria;
-// 
-//    @Column(unique = true, nullable = false, length = 50)
-//    @NotNull
-//    @Size(max = 50)
-//    private String descripcion;
-// 
-//    @Column(name = "ruta_imagen", length = 1024)
-//    @Size(max = 1024)
-//    private String rutaImagen;
-// 
-//    @Column(name = "activo")
-//    private Boolean activo;
-// 
-//}
+*/ 
+package TechShop.Tommy.domain;
+
+ 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.Data;
+
+ 
+@Data
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+ 
+    private static final long serialVersionUID = 1L;
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+ 
+    @Column(unique = true, nullable = false, length = 50)
+    @NotNull
+    @Size(max = 50)
+    private String descripcion;
+ 
+    @Column(name = "ruta_imagen", length = 1024)
+    @Size(max = 1024)
+    private String rutaImagen;
+
+    @Column(name = "activo")
+    private Boolean activo;
+
+}
