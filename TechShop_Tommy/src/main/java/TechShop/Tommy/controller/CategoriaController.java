6ce952f5d9a3bf85ab5f84e5dc4e96ec2d173ecb
ti2myy-jpg <ti2myy@gmail.com>
2026-06-12 20:59:45 -0600

    Actualizacion semana 4
    Abre la pagina pero bota en Categoria o productos

--- TechShop_Tommy/src/main/java/TechShop/Tommy/controller/CategoriaController.java
+++ TechShop_Tommy/src/main/java/TechShop/Tommy/controller/CategoriaController.java
@@ -6,55 +6,77 @@ package TechShop.Tommy.controller;
 
 import TechShop.Tommy.domain.Categoria;
 import TechShop.Tommy.service.CategoriaService;
+import jakarta.validation.Valid;
+import java.util.Locale;
+import java.util.Optional;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/categoria")
-
-
 public class CategoriaController {
-    
+
     private final CategoriaService categoriaService;
-    
-    public CategoriaController(TechShop.Tommy.service.CategoriaService categoriaService){
+    private final MessageSource messageSource;
+
+    public CategoriaController(CategoriaService categoriaService, MessageSource messageSource) {
         this.categoriaService = categoriaService;
-    }
-    
-    @GetMapping("/listado")
-    public String inicio(Model model){
-        var categorias = categoriaService.getCategorias(false);
-        model.addAttribute("categorias",categorias);
-        model.addAttribute("totalCategorias",categorias.size());
-        model.addAttribute("categoria", new Categoria());
-        return  "categoria/listado";
+        this.messageSource = messageSource;
     }
 
-    @GetMapping("/nuevo")
-    public String categoriaNuevo(Categoria categoria) {
-        return "categoria/modifica";
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";
     }
 
     @PostMapping("/guardar")
-    public String categoriaGuardar(Categoria categoria) {
-        categoriaService.save(categoria);
+    public String guardar(@Valid Categoria categoria, @RequestParam MultipartFile imagenFile, RedirectAttributes redirectAttributes) {
+
+        categoriaService.save(categoria, imagenFile);
+        redirectAttributes.addFlashAttribute("todoOk", messageSource.getMessage("mensaje.actualizado", null, Locale.getDefault()));
+
         return "redirect:/categoria/listado";
     }
 
-    @GetMapping("/eliminar/{idCategoria}")
-    public String categoriaEliminar(Categoria categoria) {
-        categoriaService.delete(categoria);
+    @PostMapping("/eliminar")
+    public String eliminar(@RequestParam Integer idCategoria, RedirectAttributes redirectAttributes) {
+        String titulo = "todoOk";
+        String detalle = "mensaje.eliminado";
+        try {
+            categoriaService.delete(idCategoria);
+        } catch (IllegalArgumentException e) {
+            titulo = "error"; // Captura la excepción de argumento inválido para el mensaje de "no existe"
+            detalle = "categoria.error01";
+        } catch (IllegalStateException e) {
+            titulo = "error"; // Captura la excepción de estado ilegal para el mensaje de "datos asociados"
+            detalle = "categoria.error02";
+        } catch (Exception e) {
+            titulo = "error";  // Captura cualquier otra excepción inesperada
+            detalle = "categoria.error03";
+        }
+        redirectAttributes.addFlashAttribute(titulo, messageSource.getMessage(detalle, null, Locale.getDefault()));
         return "redirect:/categoria/listado";
     }
 
     @GetMapping("/modificar/{idCategoria}")
-    public String categoriaModificar(Categoria categoria, Model model) {
-        categoria = categoriaService.getCategoria(categoria);
-        model.addAttribute("categoria", categoria);
-        return "categoria/modifica";
+    public String modificar(@PathVariable("idCategoria") Integer idCategoria, Model model, RedirectAttributes redirectAttributes) {
+        Optional<Categoria> categoriaOpt = categoriaService.getCategoria(idCategoria);
+        if (categoriaOpt.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", messageSource.getMessage("categoria.error01", null, Locale.getDefault()));
+            return "redirect:/categoria/listado";
+        }
+        model.addAttribute("categoria", categoriaOpt.get());
+        return "/categoria/modifica";
     }
 }
--- TechShop_Tommy/src/main/java/TechShop/Tommy/service/CategoriaService.java
+++ TechShop_Tommy/src/main/java/TechShop/Tommy/service/CategoriaService.java
@@ -4,48 +4,69 @@
  */
 
 package TechShop.Tommy.service;
- 
-import TechShop.Tommy.domain.Categoria; 
+import TechShop.Tommy.domain.Categoria;
 import TechShop.Tommy.repository.CategoriaRepository;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
- 
-@Service
+import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class CategoriaService {
 
+    // El repositorio es final para asegurar la inmutabilidad
     private final CategoriaRepository categoriaRepository;
-    public CategoriaService(CategoriaRepository categoriaRepository){
-        this.categoriaRepository = categoriaRepository;
+    private final FirebaseStorageService firebaseStorageService;
 
+    public CategoriaService(CategoriaRepository categoriaRepository, FirebaseStorageService firebaseStorageService) {
+        this.categoriaRepository = categoriaRepository;
+        this.firebaseStorageService = firebaseStorageService;
     }
 
     @Transactional(readOnly = true)
-    public List<Categoria> getCategorias(boolean activo){
-        if (activo) {
+    public List<Categoria> getCategorias(boolean activo) {
+        if (activo) { //Sólo activos...            
             return categoriaRepository.findByActivoTrue();
         }
         return categoriaRepository.findAll();
-
-    }
-     @Transactional(readOnly = true)
-    public Categoria getCategoria(Categoria categoria) {
-        return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
     }
 
-    @Transactional
-    public void save(Categoria categoria) {
-        categoriaRepository.save(categoria);
+    @Transactional(readOnly = true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
     }
 
     @Transactional
-    public void delete(Categoria categoria) {
-        categoriaRepository.delete(categoria);
+    public void save(Categoria categoria, MultipartFile imagenFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (!imagenFile.isEmpty()) { //Si no está vacío... pasaron una imagen...            
+            try {
+                String rutaImagen = firebaseStorageService.uploadImage(
+                        imagenFile, "categoria",
+                        categoria.getIdCategoria());
+                categoria.setRutaImagen(rutaImagen);
+                categoriaRepository.save(categoria);
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    @Transactional
+    public void delete(Integer idCategoria) {
+        // Verifica si la categoría existe antes de intentar eliminarlo
+        if (!categoriaRepository.existsById(idCategoria)) {
+            // Lanza una excepción para indicar que el usuario no fue encontrado
+            throw new IllegalArgumentException("La categoría con ID " + idCategoria + " no existe.");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            // Lanza una nueva excepción para encapsular el problema de integridad de datos
+            throw new IllegalStateException("No se puede eliminar la categoria. Tiene datos asociados.", e);
+        }
     }
 }
-
-
-
-
- 
